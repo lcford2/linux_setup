5c9c0943329d03bf87814468bd1eaf3cf146f95c
Lucas Ford <lcford2@ncsu.edu>
2022-06-27 07:58:24 -0400

    rofi hidpi font change

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -8,15 +8,18 @@ if [ $ndisplays -eq 2 ]; then
            --output DP-1 --pos 1920x0 --panning 1920x1080+1920+0 \
            --output DP-2-1 --pos 1920x0 --panning 1920x1080+1920+0 \
            --output DP-2-2 --pos 1920x0 --panning 1920x1080+1920+0
+    sed -i 's/font: "DejaVuSansMono Nerd Font [0-9][0-9]"/font: "DejaVuSansMono Nerd Font 16"/' $HOME/.config/rofi/config.rasi
 elif [ $ndisplays -eq 3 ]; then
     echo "3 displays" >> "/home/lford/.config/qtile/xorg.log"
     xrandr --output DP-2-3 --mode 1920x1080 \
            --output DP-2-2 --panning 1920x1080+1920+0 --right-of DP-2-3 \
            --output eDP-1 --mode 1920x1080 --panning 1920x1080+3840+0 --right-of DP-2-2
+    sed -i 's/font: "DejaVuSansMono Nerd Font [0-9][0-9]"/font: "DejaVuSansMono Nerd Font 16"/' $HOME/.config/rofi/config.rasi
 else
     echo "1 display" >> "/home/lford/.config/qtile/xorg.log"
-    # xrandr --dpi 192 --output eDP-1 --mode 3840x2160
-    xrandr --output eDP-1 --mode 1920x1080
+    xrandr --output eDP-1 --mode 3840x2160
+    sed -i 's/font: "DejaVuSansMono Nerd Font [0-9][0-9]"/font: "DejaVuSansMono Nerd Font 26"/' $HOME/.config/rofi/config.rasi
+    # xrandr --output eDP-1 --mode 1920x1080
 fi
 
 # nitrogen for wallpaper
