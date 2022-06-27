3077950eca299f720e1e01c4f938c11133201418
Lucas Ford <lcford2@ncsu.edu>
2022-06-27 07:59:34 -0400

    wifi or eth check

--- qtile/settings/net_ssid.py
+++ qtile/settings/net_ssid.py
@@ -1,6 +1,8 @@
 """Display network ssid in status bar for QTile"""
 
 import subprocess
+import re
+from datetime import datetime
 
 from libqtile import pangocffi
 from libqtile.log_utils import logger
@@ -9,7 +11,18 @@ from libqtile.widget import base
 __author__ = "Lucas Ford <lcford185@gmail.com>"
 __version__ = "0.1"
 
-ICON = " "
+WIFI_ICON = " "
+ETH_ICON = " "
+NO_I_ICON = "睊 "
+
+log_file = "/home/lford/net_ssid.log"
+
+def log_message(string, file=log_file):
+    dt = datetime.now().strftime(
+        "[%Y-%m-%d %H:%M:%S]"
+    )
+    with open("/home/lford/net_ssid.log", "a") as f:
+        f.write(f"{dt} {string}\n")
 
 # Handle the change of widget base class in the Qtile project
 BaseClass = base.ThreadPoolText
@@ -20,7 +33,7 @@ class NetSSID(BaseClass):
 
     orientations = base.ORIENTATION_HORIZONTAL
     defaults = [
-        ("update_interval", 3600, "Update interval in seconds between look ups"),
+        ("update_interval", 1, "Update interval in seconds between look ups"),
     ]
 
     def __init__(self, **config):
@@ -32,11 +45,58 @@ class NetSSID(BaseClass):
         self.add_defaults(NetSSID.defaults)
         self.markup = True
 
-    def poll(self):
+    def check_wifi(self):
         try:
-            result = subprocess.check_output(["iwgetid","-r"])
+            result = subprocess.check_output(["iwgetid", "-r"])
             ssid = result.decode().strip()
-        except subprocess.CalledProcessError as e:
-            ssid = "Error"
+            log_message(f"WIFI SSID: {ssid}")
+            return ssid
+        except subprocess.CalledProcessError:
+            return False
 
-        return f"{ICON} {ssid}"
+    def check_wired(self):
+        result = subprocess.check_output(["nmcli", "--overview", "c"])
+        res_text = result.decode().split("\n")
+        pattern = re.compile("Wired.*ethernet +(.*)")
+        match = None
+        for i in res_text:
+            m = re.search(pattern, i)
+            if m:
+                match = m
+                log_message(f"WIRED NMCLI: {match.groups(0)[0]}")
+                break
+        # get group (is returned as a tuple)
+        try:
+            interface = match.groups(1)[0].strip()
+        except AttributeError:
+            # no match
+            return False
+
+        try:
+            with open(f"/sys/class/net/{interface}/operstate", "r") as f:
+                state = f.read().strip("\n")
+            log_message(f"WIRED STATUS: {state}")
+        except FileNotFoundError as e:
+            log_message(f"WIRED STATUS: {e}")
+            return False
+
+        if state == "up":
+            return interface
+        else:
+            return False
+
+
+    def poll(self):
+        wifi_result = self.check_wifi()
+        wired_result = self.check_wired()
+        max_len = 10
+        if wifi_result:
+            if len(wifi_result) > max_len:
+                wifi_result = f"{wifi_result[:max_len]}..."
+            return f"{WIFI_ICON} {wifi_result}"
+        elif wired_result:
+            if len(wired_result) > max_len:
+                wired_result = f"{wired_result[:10]}..."
+            return f"{ETH_ICON} {wired_result}"
+        else:
+            return f"{NO_I_ICON} Error"
