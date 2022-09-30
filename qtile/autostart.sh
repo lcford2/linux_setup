93accdb38d64d9c9c30070ec3fc88ef744b222d6
Lucas Ford <lcford2@ncsu.edu>
2022-09-30 07:44:53 -0400

    change rasi font on startup

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -6,19 +6,22 @@ if [ $ndisplays -eq 2 ]; then
     echo "2 displays" >> "/home/lford/.config/qtile/xorg.log"
     xrandr --output eDP-1 --mode 1920x1080 \
            --output DP-1 --pos 1920x0 --panning 1920x1080+1920+0 \
-           --output DP-2-1 --pos 1920x0 --panning 1920x1080+1920+0 \
-           --output DP-2-2 --pos 1920x0 --panning 1920x1080+1920+0
+           # --output DP-2-1 --pos 1920x0 --panning 1920x1080+1920+0 \
+           # --output DP-2-2 --pos 1920x0 --panning 1920x1080+1920+0
     sed -i 's/font: "DejaVuSansMono Nerd Font [0-9][0-9]"/font: "DejaVuSansMono Nerd Font 16"/' $HOME/.config/rofi/config.rasi
+    sed -i 's/Xft\.dpi:.*/Xft.dpi: 96/' $HOME/.Xresources
 elif [ $ndisplays -eq 3 ]; then
     echo "3 displays" >> "/home/lford/.config/qtile/xorg.log"
     xrandr --output DP-2-3 --mode 1920x1080 \
            --output DP-2-2 --panning 1920x1080+1920+0 --right-of DP-2-3 \
            --output eDP-1 --mode 1920x1080 --panning 1920x1080+3840+0 --right-of DP-2-2
     sed -i 's/font: "DejaVuSansMono Nerd Font [0-9][0-9]"/font: "DejaVuSansMono Nerd Font 16"/' $HOME/.config/rofi/config.rasi
+    sed -i 's/Xft\.dpi:.*/Xft.dpi: 96/' $HOME/.Xresources
 else
     echo "1 display" >> "/home/lford/.config/qtile/xorg.log"
     xrandr --output eDP-1 --mode 3840x2160
     sed -i 's/font: "DejaVuSansMono Nerd Font [0-9][0-9]"/font: "DejaVuSansMono Nerd Font 26"/' $HOME/.config/rofi/config.rasi
+    sed -i 's/Xft\.dpi:.*/Xft.dpi: 192/' $HOME/.Xresources
     # xrandr --output eDP-1 --mode 1920x1080
 fi
 
@@ -39,3 +42,5 @@ emacs --daemon
 
 # start playerctl
 playerctld daemon
+
+/usr/bin/setxkbmap -option "ctrl:nocaps"
