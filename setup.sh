1a4940d6292fe11c0bcfb0358fe76d6e48321aea
Lucas Ford <lcford2@ncsu.edu>
2022-11-27 11:06:44 -0500

    kitty setup

--- setup.sh
+++ setup.sh
@@ -97,6 +97,19 @@ $CARGO install mcfly
 $CARGO install procs
 $CARGO install --locked starship
 
+#### --------------- kitty ---------------- ####
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+# Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in
+# your system-wide PATH)
+ln -sf $HOME/.local/kitty.app/bin/kitty $HOME/.local/bin/
+# Place the kitty.desktop file somewhere it can be found by the OS
+cp $HOME/.local/kitty.app/share/applications/kitty.desktop $HOME/.local/share/applications/
+# If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
+cp $HOME/.local/kitty.app/share/applications/kitty-open.desktop $HOME/.local/share/applications/
+# Update the paths to the kitty and its icon in the kitty.desktop file(s)
+sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" $HOME/.local/share/applications/kitty*.desktop
+sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" $HOME/.local/share/applications/kitty*.desktop
+
 #### --------------- fonts ---------------- ####
 pushd $HOME/Downloads
 curl -O "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/DejaVuSansMono.zip"
