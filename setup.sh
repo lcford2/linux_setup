b89a1a577726abeb5d403bb285e73b4b7994c4b3
Lucas Ford <lcford2@ncsu.edu>
2023-07-30 09:43:50 -0400

    setup.sh improvements

--- setup.sh
+++ setup.sh
@@ -13,34 +13,34 @@ function print_header() {
     # width=70
     width="$(tput cols)"
     python_command="print('#'*${width})"
-    hashes="$(python3 -c $python_command)"
+    hashes="$(python3 -c "$python_command")"
 
     echo -e "${GREEN}${hashes}"
-    echo $name | awk -v w=$width '{ z = w - length; y = int(z / 2); x = z - y; printf "%*s%s%*s\n", x, "", $0, y, ""; }'
+    echo "$name" | awk -v w="$width" '{ z = w - length; y = int(z / 2); x = z - y; printf "%*s%s%*s\n", x, "", $0, y, ""; }'
     echo -e "${GREEN}${hashes}"
     echo -e "${NC}"
 }
 
 function pushd () {
-    command pushd "$@" > /dev/null
+    command pushd "$@" > /dev/null || exit
 }
 
 function popd () {
-    command popd "$@" > /dev/null
+    command popd > /dev/null || exit
 }
 
 function safe_link () {
     target=$1
     link_name=$2
     
-    if [ -e $link_name ]; then
-        if [ -L $link_name ]; then
+    if [ -e "$link_name" ]; then
+        if [ -L "$link_name" ]; then
             echo "Cannot create link from $target to $link_name. $link_name is already a link"
         else
             echo "Cannot create link from $target to $link_name. $link_name exists but is not a link"
         fi
     else
-        ln -sfv $target $link_name
+        ln -sfv "$target" "$link_name"
     fi
 }
 
@@ -75,54 +75,53 @@ cp rofi/config.rasi.base rofi/config.rasi
 
 #### ---------- directory setup ----------- ####
 print_header "Setup Directories"
-if ! [[ -d $HOME/source ]]; then
-    mkdir -v $HOME/source
+if ! [[ -d "$HOME/source" ]]; then
+    mkdir -v "$HOME/source"
 fi
 
-if ! [[ -d $HOME/.config ]]; then
-    mkdir -v $HOME/.config
+if ! [[ -d "$HOME/.config" ]]; then
+    mkdir -v "$HOME/.config"
 fi
 
-if ! [[ -d $HOME/projects ]]; then
-    mkdir -v $HOME/projects
+if ! [[ -d "$HOME/projects" ]]; then
+    mkdir -v "$HOME/projects"
 fi
 
-if ! [[ -d $HOME/.local/bin ]]; then
-    mkdir -vp $HOME/.local/bin
+if ! [[ -d "$HOME/.local/bin" ]]; then
+    mkdir -vp "$HOME/.local/bin"
 fi
 
-if ! [[ -d $HOME/.fonts ]]; then
-    mkdir -v $HOME/.fonts
+if ! [[ -d "$HOME/.fonts" ]]; then
+    mkdir -v "$HOME/.fonts"
 fi
 
 #### ------ link config directories ------- ####
 print_header "Link Configuration Directores"
-safe_link $HOME/linux_setup/alacritty $HOME/.config/alacritty
-safe_link $HOME/linux_setup/helix     $HOME/.config/helix
-safe_link $HOME/linux_setup/kitty     $HOME/.config/kitty
-safe_link $HOME/linux_setup/qtile     $HOME/.config/qtile
-safe_link $HOME/linux_setup/rofi      $HOME/.config/rofi
-safe_link $HOME/linux_setup/htop      $HOME/.config/htop
-safe_link $HOME/linux_setup/nvim      $HOME/.config/nvim
+safe_link "$HOME/linux_setup/alacritty" "$HOME/.config/alacritty"
+safe_link "$HOME/linux_setup/helix"     "$HOME/.config/helix"
+safe_link "$HOME/linux_setup/kitty"     "$HOME/.config/kitty"
+safe_link "$HOME/linux_setup/qtile"     "$HOME/.config/qtile"
+safe_link "$HOME/linux_setup/rofi"      "$HOME/.config/rofi"
+safe_link "$HOME/linux_setup/htop"      "$HOME/.config/htop"
+safe_link "$HOME/linux_setup/nvim"      "$HOME/.config/nvim"
 
 # link vim and doom directories (in $HOME)
-safe_link $HOME/linux_setup/.doom.d    $HOME/.doom.d
-safe_link $HOME/linux_setup/vimrc/.vim $HOME/.vim
+safe_link "$HOME/linux_setup/.doom.d"    "$HOME/.doom.d"
+safe_link "$HOME/linux_setup/vimrc/.vim" "$HOME/.vim"
 
 # link starship.toml file
-safe_link $HOME/linux_setup/misc_config/starship.toml $HOME/.config/starship.toml
+safe_link "$HOME/linux_setup/misc_config/starship.toml" "$HOME/.config/starship.toml"
 
 # link git setup
->>>>>>> b47e570bb3d7f73ff70d9ef1ff1ae0bbbbe1cad6
-safe_link $HOME/linux_setup/git/.gitconfig $HOME/.gitconfig
-safe_link $HOME/linux_setup/git/.gitignore $HOME/.gitignore
+safe_link "$HOME/linux_setup/git/.gitconfig" "$HOME/.gitconfig"
+safe_link "$HOME/linux_setup/git/.gitignore" "$HOME/.gitignore"
 
 # link zshrc
-safe_link $HOME/linux_setup/zsh/.zshrc $HOME/.zshrc
+safe_link "$HOME/linux_setup/zsh/.zshrc" "$HOME/.zshrc"
 
 # link script to .local/bin
-for file in $(/bin/ls $HOME/linux_setup/scripts/); do
-    ln -sf $HOME/linux_setup/scripts/$file $HOME/.local/bin/$file
+for file in $(/bin/ls "$HOME"/linux_setup/scripts/); do
+    ln -sf "$HOME/linux_setup/scripts/$file" "$HOME/.local/bin/$file"
 done
 
 
@@ -140,15 +139,15 @@ sudo apt build-dep -y emacs
 sudo apt install -y libgccjit0 libgccjit-10-dev libjansson4 libjansson-dev gnutls-bin
 # get emacs
 if ! command -v emacs &> /dev/null || [ "$UPDATE" -eq 1 ]; then
-    pushd $HOME/source
+    pushd "$HOME/source" || exit
     curl -O "https://ftp.gnu.org/pub/gnu/emacs/emacs-${EMACS_VERSION}.tar.xz"
-    tar xf emacs-$EMACS_VERSION.tar.xz
+    tar xf "emacs-$EMACS_VERSION.tar.xz"
     # build emacs
-    cd emacs-$EMACS_VERSION
+    cd "emacs-$EMACS_VERSION" || exit
     ./configure --with-x-toolkit=lucid
     make -j 4
     sudo make install
-    popd
+    popd || exit
 fi
 
 # doom emacs
@@ -161,15 +160,15 @@ fi
 #### ---------------- nvim ---------------- ####
 print_header "Install NeoVim"
 if ! command -v nvim &> /dev/null || [ "$UPDATE" -eq 1 ]; then
-    pushd $HOME/Downloads
+    pushd "$HOME/Downloads" || exit
     wget "https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb" -O "nvim-linux64.deb"
     sudo apt install -y ./nvim-linux64.deb
-    popd
+    popd || exit
 fi
 
 #### -------------- vundle ---------------- ####
 print_header "Install Vundle for NeoVim"
-if [ "$(ls -A1 $HOME/.vim/bundle/Vundle.vim | wc -l)" -eq 0 ]; then
+if [ "$(ls -A1 "$HOME/.vim/bundle/Vundle.vim" | wc -l)" -eq 0 ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
@@ -198,19 +197,19 @@ if ! command -v kitty &> /dev/null || [ "$UPDATE" -eq 1 ]; then
     curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
     # Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in
     # your system-wide PATH)
-    ln -sf $HOME/.local/kitty.app/bin/kitty $HOME/.local/bin/
+    ln -sf "$HOME/.local/kitty.app/bin/kitty" "$HOME/.local/bin/"
     # Place the kitty.desktop file somewhere it can be found by the OS
-    cp $HOME/.local/kitty.app/share/applications/kitty.desktop $HOME/.local/share/applications/
+    cp "$HOME/.local/kitty.app/share/applications/kitty.desktop" "$HOME/.local/share/applications/"
     # If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
-    cp $HOME/.local/kitty.app/share/applications/kitty-open.desktop $HOME/.local/share/applications/
+    cp "$HOME/.local/kitty.app/share/applications/kitty-open.desktop" "$HOME/.local/share/applications/"
     # Update the paths to the kitty and its icon in the kitty.desktop file(s)
-    sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" $HOME/.local/share/applications/kitty*.desktop
-    sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" $HOME/.local/share/applications/kitty*.desktop
+    sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" "$HOME/.local/share/applications/kitty*.desktop"
+    sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" "$HOME/.local/share/applications/kitty*.desktop"
 fi
 
 #### --------------- fonts ---------------- ####
 print_header "Install NerdFonts"
-pushd $HOME/Downloads
+pushd "$HOME/Downloads" || exit
 wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v$NERDFONT_VERSION/DejaVuSansMono.zip" -O "DejaVuSansMono.zip"
 wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v$NERDFONT_VERSION/UbuntuMono.zip" -O "UbuntuMono.zip"
 wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v$NERDFONT_VERSION/Ubuntu.zip" -O "Ubuntu.zip"
@@ -218,43 +217,43 @@ unzip -u DejaVuSansMono.zip
 unzip -u UbuntuMono.zip
 unzip -u Ubuntu.zip
 
-mv *.ttf $HOME/.fonts
+mv "*.ttf" "$HOME/.fonts"
 sudo fc-cache -f -v
-popd
+popd || exit
 
 
 #### ---------------- zsh ----------------- ####
 print_header "Install and Configure ZSH"
 if ! command -v zsh &> /dev/null || [ "$UPDATE" -eq 1 ]; then
-    pushd $HOME/source
+    pushd "$HOME/source" || exit
     wget "https://gigenet.dl.sourceforge.net/project/zsh/zsh/$ZSH_VERSION/zsh-$ZSH_VERSION.tar.xz" -O "zsh-$ZSH_VERSION.tar.xz"
-    tar xf zsh-$ZSH_VERSION.tar.xz
-    cd zsh-$ZSH_VERSION
+    tar xf "zsh-$ZSH_VERSION.tar.xz"
+    cd "zsh-$ZSH_VERSION" || exit
     ./configure && make && sudo make install
-    popd
+    popd || exit
 fi
 
 # zsh autosuggestions
-pushd ~/source
+pushd ~/source || exit
 if ! [ -d zsh-syntax-highlighting ]; then
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 else
-    cd zsh-syntax-highlighting
+    cd zsh-syntax-highlighting || exit
     git pull
     cd ..
 fi
 if ! [ -d zsh-autosuggestions ]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions.git
 else
-    cd zsh-autosuggestions
+    cd zsh-autosuggestions || exit
     git pull
     cd ..
 fi
-popd
+popd || exit
 
 # history file
-if ! [ -f $HOME/.zshhistory ]; then
-    touch $HOME/.zshhistory
+if ! [ -f "$HOME/.zshhistory" ]; then
+    touch "$HOME/.zshhistory"
 fi
 
 # add zsh to shells
@@ -266,30 +265,30 @@ fi
 #### --------------- pyenv ---------------- ####
 print_header "Install pyenv"
 if ! [[ -d $HOME/.pyenv ]]; then
-    git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
-    pushd $HOME/.pyenv
+    git clone https://github.com/pyenv/pyenv.git "$HOME/.pyenv"
+    pushd "$HOME/.pyenv" || exit
     src/configure && make -C src
-    popd
+    popd || exit
 fi
 
 #### --------------- conda ---------------- ####
 print_header "Install Miniconda"
 if ! command -v conda &> /dev/null; then
-    cd $HOME/Downloads
+    cd "$HOME/Downloads" || exit
     MINICONDA_FILE="Miniconda3-latest-Linux-x86_64.sh"
-    wget "https://repo.anaconda.com/miniconda/$MINICONDA_FILE" -O $MINICONDA_FILE
-    sh $MINICONDA_FILE -b -p $HOME/miniconda3
-    $HOME/miniconda3/condabin/conda init
-    popd
+    wget "https://repo.anaconda.com/miniconda/$MINICONDA_FILE" -O "$MINICONDA_FILE"
+    sh "$MINICONDA_FILE" -b -p "$HOME/miniconda3"
+    "$HOME"/miniconda3/condabin/conda init
+    popd || exit
 fi
 
 #### ------------- doom emacs ------------- ####
-if [ ! -f $HOME/.emacs.d/bin/doom ]; then
-    if [ -d $HOME/.emacs.d ]; then
-        mv $HOME/.emacs.d $HOME/.emacs.d/bkp
+if [ ! -f "$HOME/.emacs.d/bin/doom" ]; then
+    if [ -d "$HOME/.emacs.d" ]; then
+        mv "$HOME/.emacs.d" "$HOME/.emacs.d/bkp"
     fi
-    git clone --depth 1 https://github.com/doomemacs/doomemacs $HOME/.emacs.d
-    $HOME/.emacs.d/bin/doom install
+    git clone --depth 1 https://github.com/doomemacs/doomemacs "$HOME/.emacs.d"
+    "$HOME"/.emacs.d/bin/doom install
 else
-    $HOME/.emacs.d/bin/doom sync
+    "$HOME"/.emacs.d/bin/doom sync
 fi
