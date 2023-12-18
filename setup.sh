f67b7f0854968d5863f305a26356555903eaf394
Lucas Ford <lcford2@ncsu.edu>
2023-12-18 07:55:38 -0500

    macos specific tweaks for computer setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-EMACS_VERSION="28.2"
 ZSH_VERSION="5.9"
 NERDFONT_VERSION="2.2.2"
 
@@ -32,12 +31,12 @@ function popd () {
 function safe_link () {
     target=$1
     link_name=$2
-    
+
     if [ -e "$link_name" ]; then
         if [ -L "$link_name" ]; then
-            echo "Cannot create link from $target to $link_name. $link_name is already a link"
+            echo "${RED}Cannot create link from $target to $link_name. $link_name is already a link${NC}"
         else
-            echo "Cannot create link from $target to $link_name. $link_name exists but is not a link"
+            echo "${RED}Cannot create link from $target to $link_name. $link_name exists but is not a link${NC}"
         fi
     else
         ln -sfv "$target" "$link_name"
@@ -60,12 +59,14 @@ function parse_args () {
 
 parse_args "$@"
 
-#### ----------- system update ------------ ####
-# update system and install curl
-# download and install various programs
-print_header "System Update"
-sudo apt update && sudo apt upgrade
-sudo apt install -y curl cmake build-essential gcp pbzip2 tk htop libssl-dev libsqlite3-dev openssl cifs-utils smbclient libreadline8 libreadline-dev tk tk-dev
+#### ---------- Install Homebrew ---------- ####
+print_header "Install Homebrew"
+if ! command -v brew &> /dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+print_header "Install Packages with brew"
+brew install google-chrome visual-studio-code emacs neovim starship htop rectangle
 
 #### ------- program configurations ------- ####
 print_header "Create Computer Specific Program Configurations"
@@ -124,47 +125,45 @@ for file in $(/bin/ls "$HOME"/linux_setup/scripts/); do
     ln -sf "$HOME/linux_setup/scripts/$file" "$HOME/.local/bin/$file"
 done
 
-
 #### --------------- rustup --------------- ####
 print_header "Install Rustup"
 # if ! command -v rustup &> /dev/null || [ "$UPDATE" -eq 1 ]; then
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # fi
 
-
 #### --------------- emacs ---------------- ####
-print_header "Build Emacs $EMACS_VERSION"
-# build deps
-sudo apt build-dep -y emacs
-sudo apt install -y libgccjit0 libgccjit-10-dev libjansson4 libjansson-dev gnutls-bin
-# get emacs
-if ! command -v emacs &> /dev/null || [ "$UPDATE" -eq 1 ]; then
-    pushd "$HOME/source" || exit
-    curl -O "https://ftp.gnu.org/pub/gnu/emacs/emacs-${EMACS_VERSION}.tar.xz"
-    tar xf "emacs-$EMACS_VERSION.tar.xz"
-    # build emacs
-    cd "emacs-$EMACS_VERSION" || exit
-    ./configure --with-x-toolkit=lucid
-    make -j 4
-    sudo make install
-    popd || exit
-fi
+# print_header "Build Emacs $EMACS_VERSION"
+# # build deps
+# sudo apt build-dep -y emacs
+# sudo apt install -y libgccjit0 libgccjit-10-dev libjansson4 libjansson-dev gnutls-bin
+# # get emacs
+# if ! command -v emacs &> /dev/null || [ "$UPDATE" -eq 1 ]; then
+#     pushd "$HOME/source" || exit
+#     curl -O "https://ftp.gnu.org/pub/gnu/emacs/emacs-${EMACS_VERSION}.tar.xz"
+#     tar xf "emacs-$EMACS_VERSION.tar.xz"
+#     # build emacs
+#     cd "emacs-$EMACS_VERSION" || exit
+#     ./configure --with-x-toolkit=lucid
+#     make -j 4
+#     sudo make install
+#     popd || exit
+# fi
 
-# doom emacs
-print_header "Install Doom Emacs"
-if ! [ -d "$HOME/.emacs.d" ]; then
-    git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
-    ~/.emacs.d/bin/doom install
-fi
+# # doom emacs
+# print_header "Install Doom Emacs"
+# if ! [ -d "$HOME/.emacs.d" ]; then
+#     git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
+#     ~/.emacs.d/bin/doom install
+# fi
 
 #### ---------------- nvim ---------------- ####
-print_header "Install NeoVim"
-if ! command -v nvim &> /dev/null || [ "$UPDATE" -eq 1 ]; then
-    pushd "$HOME/Downloads" || exit
-    wget "https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb" -O "nvim-linux64.deb"
-    sudo apt install -y ./nvim-linux64.deb
-    popd || exit
-fi
+# print_header "Install NeoVim"
+# if ! command -v nvim &> /dev/null || [ "$UPDATE" -eq 1 ]; then
+#     pushd "$HOME/Downloads" || exit
+#     wget "https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb" -O "nvim-linux64.deb"
+#     sudo apt install -y ./nvim-linux64.deb
+#     popd || exit
+# fi
 
 #### -------------- vundle ---------------- ####
 print_header "Install Vundle for NeoVim"
@@ -195,18 +194,9 @@ fi
 print_header "Install Kitty"
 if ! command -v kitty &> /dev/null || [ "$UPDATE" -eq 1 ]; then
     curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-    # Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in
-    # your system-wide PATH)
-    ln -sf "$HOME/.local/kitty.app/bin/kitty" "$HOME/.local/bin/"
-    # Place the kitty.desktop file somewhere it can be found by the OS
-    cp "$HOME/.local/kitty.app/share/applications/kitty.desktop" "$HOME/.local/share/applications/"
-    # If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
-    cp "$HOME/.local/kitty.app/share/applications/kitty-open.desktop" "$HOME/.local/share/applications/"
-    # Update the paths to the kitty and its icon in the kitty.desktop file(s)
-    sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" "$HOME/.local/share/applications/kitty*.desktop"
-    sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" "$HOME/.local/share/applications/kitty*.desktop"
 fi
 
+exit
 #### --------------- fonts ---------------- ####
 print_header "Install NerdFonts"
 pushd "$HOME/Downloads" || exit
@@ -221,74 +211,51 @@ mv "*.ttf" "$HOME/.fonts"
 sudo fc-cache -f -v
 popd || exit
 
-
-#### ---------------- zsh ----------------- ####
-print_header "Install and Configure ZSH"
-if ! command -v zsh &> /dev/null || [ "$UPDATE" -eq 1 ]; then
-    pushd "$HOME/source" || exit
-    wget "https://gigenet.dl.sourceforge.net/project/zsh/zsh/$ZSH_VERSION/zsh-$ZSH_VERSION.tar.xz" -O "zsh-$ZSH_VERSION.tar.xz"
-    tar xf "zsh-$ZSH_VERSION.tar.xz"
-    cd "zsh-$ZSH_VERSION" || exit
-    ./configure && make && sudo make install
-    popd || exit
-fi
-
 # zsh autosuggestions
-pushd ~/source || exit
-if ! [ -d zsh-syntax-highlighting ]; then
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-else
-    cd zsh-syntax-highlighting || exit
-    git pull
-    cd ..
-fi
-if ! [ -d zsh-autosuggestions ]; then
-    git clone https://github.com/zsh-users/zsh-autosuggestions.git
-else
-    cd zsh-autosuggestions || exit
-    git pull
-    cd ..
-fi
-popd || exit
-
-# history file
-if ! [ -f "$HOME/.zshhistory" ]; then
-    touch "$HOME/.zshhistory"
-fi
-
-# add zsh to shells
-if [ "$(/bin/grep "/usr/local/bin/zsh" /etc/shells -c)" -eq 0 ]; then
-    echo "/usr/local/bin/zsh" | sudo tee -a /etc/shells
-fi
-
+# pushd ~/source || exit
+# if ! [ -d zsh-syntax-highlighting ]; then
+#     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+# else
+#     cd zsh-syntax-highlighting || exit
+#     git pull
+#     cd ..
+# fi
+# if ! [ -d zsh-autosuggestions ]; then
+#     git clone https://github.com/zsh-users/zsh-autosuggestions.git
+# else
+#     cd zsh-autosuggestions || exit
+#     git pull
+#     cd ..
+# fi
+# popd || exit
 
 #### --------------- pyenv ---------------- ####
-print_header "Install pyenv"
-if ! [[ -d $HOME/.pyenv ]]; then
-    git clone https://github.com/pyenv/pyenv.git "$HOME/.pyenv"
-    pushd "$HOME/.pyenv" || exit
-    src/configure && make -C src
-    popd || exit
-fi
+# print_header "Install pyenv"
+# if ! [[ -d $HOME/.pyenv ]]; then
+#     git clone https://github.com/pyenv/pyenv.git "$HOME/.pyenv"
+#     pushd "$HOME/.pyenv" || exit
+#     src/configure && make -C src
+#     popd || exit
+# fi
 
 #### --------------- conda ---------------- ####
-print_header "Install Miniconda"
-if ! command -v conda &> /dev/null; then
-    cd "$HOME/Downloads" || exit
-    MINICONDA_FILE="Miniconda3-latest-Linux-x86_64.sh"
-    wget "https://repo.anaconda.com/miniconda/$MINICONDA_FILE" -O "$MINICONDA_FILE"
-    sh "$MINICONDA_FILE" -b -p "$HOME/miniconda3"
-    "$HOME"/miniconda3/condabin/conda init
-    popd || exit
-fi
+# print_header "Install Miniconda"
+# if ! command -v conda &> /dev/null; then
+#     cd "$HOME/Downloads" || exit
+#     MINICONDA_FILE="Miniconda3-latest-Linux-x86_64.sh"
+#     wget "https://repo.anaconda.com/miniconda/$MINICONDA_FILE" -O "$MINICONDA_FILE"
+#     sh "$MINICONDA_FILE" -b -p "$HOME/miniconda3"
+#     "$HOME"/miniconda3/condabin/conda init
+#     popd || exit
+# fi
 
 #### ------------- doom emacs ------------- ####
-if [ ! -f "$HOME/.emacs.d/bin/doom" ]; then
-    if [ -d "$HOME/.emacs.d" ]; then
-        mv "$HOME/.emacs.d" "$HOME/.emacs.d/bkp"
-    fi
-    git clone --depth 1 https://github.com/doomemacs/doomemacs "$HOME/.emacs.d"
-    "$HOME"/.emacs.d/bin/doom install
-else
-    "$HOME"/.emacs.d/bin/doom sync
-fi
+# if [ ! -f "$HOME/.emacs.d/bin/doom" ]; then
+#     if [ -d "$HOME/.emacs.d" ]; then
+#         mv "$HOME/.emacs.d" "$HOME/.emacs.d/bkp"
+#     fi
+#     git clone --depth 1 https://github.com/doomemacs/doomemacs "$HOME/.emacs.d"
+#     "$HOME"/.emacs.d/bin/doom install
+# else
+#     "$HOME"/.emacs.d/bin/doom sync
+# fi
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,29 +1,40 @@
 # History
 
+export KITTY_CONFIG_DIRECTORY="~/.config/kitty"
 export HISTSIZE=1000000
 export SAVEHIST=$HISTSIZE
 export HISTFILE=~/.zshhistory
 setopt EXTENDED_HISTORY
 
+# Configure ZAP
+[ -f "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh" ] && source "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh"
+plug "zsh-users/zsh-autosuggestions"
+plug "zap-zsh/supercharge"
+# plug "zap-zsh/zap-prompt"
+plug "zsh-users/zsh-syntax-highlighting"
+plug "greymd/docker-zsh-completion"
+
+# Load and initialise completion system
+autoload -Uz compinit
+compinit
+
 # completion
-zstyle ':completion:*' completer _complete _ignored
-autoload -U compinit
-compinit
-zstyle ':completion:*' use-cache on
-zstyle ':completion:*' cache-path "$XGD_CACHE_HOME/zsh/.zcompcache"
-zstyle ':completion:*' menu select
-zstyle ':completion:*:*:docker:*' option-stacking yes
-zstyle ':completion:*:*:docker-*:*' option-stacking yes
-fpath+=~/.zfunc
+# zstyle ':completion:*' completer _complete _ignored
+# autoload -U compinit
+# compinit
+# zstyle ':completion:*' use-cache on
+# zstyle ':completion:*' cache-path "$XGD_CACHE_HOME/zsh/.zcompcache"
+# zstyle ':completion:*' menu select
+# zstyle ':completion:*:*:docker:*' option-stacking yes
+# zstyle ':completion:*:*:docker-*:*' option-stacking yes
+# fpath+=~/.zfunc
 
-autoload -U +X compinit
-compinit
+# autoload -U +X compinit
+# compinit
 
-autoload -U +X bashcompinit && bashcompinit
-eval "$(register-python-argcomplete3 ros2)"
-eval "$(register-python-argcomplete3 colcon)"
+# autoload -U +X bashcompinit && bashcompinit
 
-source ~/source/zsh-autosuggestions/zsh-autosuggestions.zsh
+# source ~/source/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 # key style
 # bindkey -e
@@ -66,26 +77,14 @@ if [[ $TERM == "xterm-kitty" ]]; then
 fi
 
 # alias vim
-export EDITOR="/usr/bin/nvim"
+export EDITOR="$(which nvim)"
 alias vim="$EDITOR"
 alias vi="$EDITOR"
 
-# pyenv
-export PYENV_ROOT=$HOME/.pyenv
-export PATH=$PYENV_ROOT/bin:$PATH
 
 if [ -f $HOME/.autojump/share/autojump/autojump.zsh ]; then
     source $HOME/.autojump/share/autojump/autojump.zsh
 fi
-
-export DIRENV_LOG_FORMAT=""
-eval "$(direnv hook zsh)"
-
-export EDITOR="$(which nvim)"
-
-# export PYENV_ROOT="$HOME/.pyenv"
-# command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-# eval "$(pyenv init -)"
 
 export MCFLY_KEY_SCHEME=vim
 export MCFLY_FUZZY=2
@@ -94,65 +93,16 @@ export MCFLY_RESULTS_SORT=LAST_RUN
 export MCFLY_HISTORY_LIMIT=10000
 eval "$(mcfly init zsh)"
 
-
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-
 export FAST_DOWNWARD_PATH="/home/lucas/dev/wall_panels/downward"
 export ROS_DOMAIN_ID=44
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-alias cdw="cd $HOME/volume_dev/wall_panels/bb_ws"
+alias cdw="cd $HOME/dev/wall_panels/bb_ws"
 alias bbstart="sudo docker exec botbuilt-amd64-desktop-devel /home/developer/.local/bin/kitty"
 
-function config-bb-direnv () {
-    pushd $HOME/dev/wall_panels
-    direnv deny .
-    source bb_ws/install/setup.zsh
-    direnv dump > .envrc.cache
-    direnv allow .
-    popd
-}
-
-function botbuild () {
-    colcon build --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_CXX_FLAGS=-w --symlink-install && \
-      config-bb-direnv
-}
-
-
-
-# Isaac Sim Python
-alias omni-python="$HOME/.local/share/ov/pkg/isaac_sim-2022.1.1/python.sh"
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/lucas/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/home/lucas/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/lucas/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/home/lucas/miniconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
-
-# cuda setup
-if [ -d /usr/local/cuda ]; then
-  export PATH=/usr/loca/cuda/bin:$PATH
-fi
-
-# gurobi setup
-export GUROBI_HOME="/opt/gurobi1000/linux64"
-export PATH="${PATH}:${GUROBI_HOME}/bin"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
-
-# tesseract setup
-export TESSDATA_PREFIX="${HOME}/.tesseract-data"
 
 # math function
 math() {
@@ -170,6 +120,8 @@ if [ -d "$HOME/balena-cli" ]; then
   export PATH="$PATH:$HOME/balena-cli"
 fi
 
-source $HOME/source/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+export PIPENV_PYTHON="$PYENV_ROOT/shims/python"
 
-source /home/lucas/.config/broot/launcher/bash/br
+eval "$(pyenv init -)"
