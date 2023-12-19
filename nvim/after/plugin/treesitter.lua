bd3fc34454309f5f90af5a0db313b33f3e3ac956
Lucas Ford <lcford2@ncsu.edu>
2023-12-19 18:12:44 -0500

    feat: add better remaps and use doge for doc gen

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -5,11 +5,11 @@
 -- Plugin: nvim-treesitter
 -- url: https://github.com/nvim-treesitter/nvim-treesitter
 
-
 local status_ok, nvim_treesitter = pcall(require, 'nvim-treesitter.configs')
 if not status_ok then
   return
 end
+
 
 -- See: https://github.com/nvim-treesitter/nvim-treesitter#quickstart
 nvim_treesitter.setup {
--- nvim/lua/base/init.lua
+++ nvim/lua/base/init.lua
@@ -3,3 +3,7 @@ require("base.remap")
 require("base.lazy")
 require("base.colors")
 require("base.statusline")
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
