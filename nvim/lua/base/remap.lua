37532a285eea7c41119c3ed2d987bd16c3b5e2c4
Lucas Ford <lcford2@ncsu.edu>
2023-12-21 21:18:12 -0500

    feat: add ratio based height and width resize commands

--- nvim/lua/base/remap.lua
+++ nvim/lua/base/remap.lua
@@ -37,3 +37,44 @@ function OpenTerminal()
 end
 
 vim.keymap.set("n", "<leader>ot", OpenTerminal)
+
+function GetVimWidth()
+  -- get the total width of the nvim window
+  local info = vim.fn.getwininfo()
+  local width = 0
+  -- sum over all nvim windows to get the total width
+  for window in pairs(info) do
+      local window_info = info[window]
+      width = width + window_info.width
+  end
+  return width
+end
+
+function GetVimHeight()
+  -- get the total height of the nvim window
+  local info = vim.fn.getwininfo()
+  local height = 0
+  -- sum over all nvim windows to get the total height
+  for window in pairs(info) do
+      local window_info = info[window]
+      height = height + window_info.height
+  end
+  return height
+end
+
+function SetWindowWidthAsRatio(ratio)
+  -- set the current window width as a ratio of the total width
+  local total_width = GetVimWidth()
+  local new_width = math.floor(ratio * total_width)
+  vim.cmd("vertical resize " .. tostring(new_width))
+end
+
+function SetWindowHeightAsRatio(ratio)
+  -- set the current window height as a ratio of the total width
+  local total_height = GetVimHeight()
+  local new_height = math.floor(ratio * total_height)
+  vim.cmd("resize " .. tostring(new_height))
+end
+
+vim.keymap.set("n", "<leader>wW", function() SetWindowWidthAsRatio(0.8) end, { desc = "Set window to 80% of total width" })
+vim.keymap.set("n", "<leader>wV", function() SetWindowHeightAsRatio(0.8) end, { desc = "Set window to 80% of total height" })
