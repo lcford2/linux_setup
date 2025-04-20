6a458831cd94cfc4c5fac70a6bda159306aa9f23
Lucas Ford <lucas@botbuilt.com>
2025-04-20 10:23:53 -0400

    feat: migrate setup script to dotfile setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-ZSH_VERSION="5.9"
 NERDFONT_VERSION="2.2.2"
 
 GREEN="\e[0;32m"
@@ -8,65 +7,49 @@ RED="\e[0;31m"
 NC="\e[0m"
 
 function print_header() {
-    name=$1
-    # width=70
-    width="$(tput cols)"
-    python_command="print('#'*${width})"
-    hashes="$(python3 -c "$python_command")"
+  name=$1
+  # width=70
+  width="$(tput cols)"
+  python_command="print('#'*${width})"
+  hashes="$(python3 -c "$python_command")"
 
-    echo -e "${GREEN}${hashes}"
-    echo "$name" | awk -v w="$width" '{ z = w - length; y = int(z / 2); x = z - y; printf "%*s%s%*s\n", x, "", $0, y, ""; }'
-    echo -e "${GREEN}${hashes}"
-    echo -e "${NC}"
+  echo -e "${GREEN}${hashes}"
+  echo "$name" | awk -v w="$width" '{ z = w - length; y = int(z / 2); x = z - y; printf "%*s%s%*s\n", x, "", $0, y, ""; }'
+  echo -e "${GREEN}${hashes}"
+  echo -e "${NC}"
 }
 
-function pushd () {
-    command pushd "$@" > /dev/null || exit
+function pushd() {
+  command pushd "$@" >/dev/null || exit
 }
 
-function popd () {
-    command popd > /dev/null || exit
+function popd() {
+  command popd >/dev/null || exit
 }
 
-function safe_link () {
-    target=$1
-    link_name=$2
-
-    if [ -e "$link_name" ]; then
-        if [ -L "$link_name" ]; then
-            echo "${RED}Cannot create link from $target to $link_name. $link_name is already a link${NC}"
-        else
-            echo "${RED}Cannot create link from $target to $link_name. $link_name exists but is not a link${NC}"
-        fi
-    else
-        ln -sfv "$target" "$link_name"
-    fi
-}
-
-function parse_args () {
-    export UPDATE=0
-    while test $# -gt 0
-    do
-        case "$1" in
-        -U|--update)
-            echo "Updating packages if existing"
-            export UPDATE=1
-            ;;
-        esac
-        shift
-    done
+function parse_args() {
+  export UPDATE=0
+  while test $# -gt 0; do
+    case "$1" in
+    -U | --update)
+      echo "Updating packages if existing"
+      export UPDATE=1
+      ;;
+    esac
+    shift
+  done
 }
 
 parse_args "$@"
 
 #### ---------- Install Homebrew ---------- ####
 print_header "Install Homebrew"
-if ! command -v brew &> /dev/null; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! command -v brew &>/dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 print_header "Install Packages with brew"
-brew install google-chrome visual-studio-code emacs neovim starship htop rectangle
+brew install google-chrome visual-studio-code neovim htop rectangle
 
 #### ------- program configurations ------- ####
 print_header "Create Computer Specific Program Configurations"
@@ -77,52 +60,32 @@ cp rofi/config.rasi.base rofi/config.rasi
 #### ---------- directory setup ----------- ####
 print_header "Setup Directories"
 if ! [[ -d "$HOME/source" ]]; then
-    mkdir -v "$HOME/source"
+  mkdir -v "$HOME/source"
 fi
 
 if ! [[ -d "$HOME/.config" ]]; then
-    mkdir -v "$HOME/.config"
+  mkdir -v "$HOME/.config"
 fi
 
 if ! [[ -d "$HOME/projects" ]]; then
-    mkdir -v "$HOME/projects"
+  mkdir -v "$HOME/projects"
 fi
 
 if ! [[ -d "$HOME/.local/bin" ]]; then
-    mkdir -vp "$HOME/.local/bin"
+  mkdir -vp "$HOME/.local/bin"
 fi
 
 if ! [[ -d "$HOME/.fonts" ]]; then
-    mkdir -v "$HOME/.fonts"
+  mkdir -v "$HOME/.fonts"
 fi
 
 #### ------ link config directories ------- ####
 print_header "Link Configuration Directores"
-safe_link "$HOME/linux_setup/alacritty" "$HOME/.config/alacritty"
-safe_link "$HOME/linux_setup/helix"     "$HOME/.config/helix"
-safe_link "$HOME/linux_setup/kitty"     "$HOME/.config/kitty"
-safe_link "$HOME/linux_setup/qtile"     "$HOME/.config/qtile"
-safe_link "$HOME/linux_setup/rofi"      "$HOME/.config/rofi"
-safe_link "$HOME/linux_setup/htop"      "$HOME/.config/htop"
-safe_link "$HOME/linux_setup/nvim"      "$HOME/.config/nvim"
-
-# link vim and doom directories (in $HOME)
-safe_link "$HOME/linux_setup/.doom.d"    "$HOME/.doom.d"
-safe_link "$HOME/linux_setup/vimrc/.vim" "$HOME/.vim"
-
-# link starship.toml file
-safe_link "$HOME/linux_setup/misc_config/starship.toml" "$HOME/.config/starship.toml"
-
-# link git setup
-safe_link "$HOME/linux_setup/git/.gitconfig" "$HOME/.gitconfig"
-safe_link "$HOME/linux_setup/git/.gitignore" "$HOME/.gitignore"
-
-# link zshrc
-safe_link "$HOME/linux_setup/zsh/.zshrc" "$HOME/.zshrc"
+stow -R -t ../. dotfiles -v 3
 
 # link script to .local/bin
 for file in $(/bin/ls "$HOME"/linux_setup/scripts/); do
-    ln -sf "$HOME/linux_setup/scripts/$file" "$HOME/.local/bin/$file"
+  ln -sf "$HOME/linux_setup/scripts/$file" "$home/.local/bin/$file"
 done
 
 #### --------------- rustup --------------- ####
@@ -131,72 +94,24 @@ print_header "Install Rustup"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # fi
 
-#### --------------- emacs ---------------- ####
-# print_header "Build Emacs $EMACS_VERSION"
-# # build deps
-# sudo apt build-dep -y emacs
-# sudo apt install -y libgccjit0 libgccjit-10-dev libjansson4 libjansson-dev gnutls-bin
-# # get emacs
-# if ! command -v emacs &> /dev/null || [ "$UPDATE" -eq 1 ]; then
-#     pushd "$HOME/source" || exit
-#     curl -O "https://ftp.gnu.org/pub/gnu/emacs/emacs-${EMACS_VERSION}.tar.xz"
-#     tar xf "emacs-$EMACS_VERSION.tar.xz"
-#     # build emacs
-#     cd "emacs-$EMACS_VERSION" || exit
-#     ./configure --with-x-toolkit=lucid
-#     make -j 4
-#     sudo make install
-#     popd || exit
-# fi
-
-# # doom emacs
-# print_header "Install Doom Emacs"
-# if ! [ -d "$HOME/.emacs.d" ]; then
-#     git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
-#     ~/.emacs.d/bin/doom install
-# fi
-
-#### ---------------- nvim ---------------- ####
-# print_header "Install NeoVim"
-# if ! command -v nvim &> /dev/null || [ "$UPDATE" -eq 1 ]; then
-#     pushd "$HOME/Downloads" || exit
-#     wget "https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb" -O "nvim-linux64.deb"
-#     sudo apt install -y ./nvim-linux64.deb
-#     popd || exit
-# fi
-
-#### -------------- vundle ---------------- ####
-print_header "Install Vundle for NeoVim"
-if [ "$(ls -A1 "$HOME/.vim/bundle/Vundle.vim" | wc -l)" -eq 0 ]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
-
 #### ---------- modern utilities ---------- ####
 print_header "Install Modern Linux Utilities"
 CARGO=$HOME/.cargo/bin/cargo
 
 if [ "$UPDATE" -eq 1 ]; then
-    cargo install-update -a
+  cargo install-update -a
 else
-    $CARGO install cargo-update
-    $CARGO install --locked bat
-    $CARGO install git-delta
-    $CARGO install du-dust
-    $CARGO install exa
-    $CARGO install fd-find
-    $CARGO install mcfly
-    $CARGO install procs
-    $CARGO install --locked starship
-    $CARGO install ripgrep
+  $CARGO install cargo-update
+  $CARGO install --locked bat
+  $CARGO install git-delta
+  $CARGO install du-dust
+  $CARGO install eza
+  $CARGO install fd-find
+  $CARGO install mcfly
+  $CARGO install procs
+  $CARGO install ripgrep
 fi
 
-#### --------------- kitty ---------------- ####
-print_header "Install Kitty"
-if ! command -v kitty &> /dev/null || [ "$UPDATE" -eq 1 ]; then
-    curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-fi
-
-exit
 #### --------------- fonts ---------------- ####
 print_header "Install NerdFonts"
 pushd "$HOME/Downloads" || exit
@@ -210,52 +125,3 @@ unzip -u Ubuntu.zip
 mv "*.ttf" "$HOME/.fonts"
 sudo fc-cache -f -v
 popd || exit
-
-# zsh autosuggestions
-# pushd ~/source || exit
-# if ! [ -d zsh-syntax-highlighting ]; then
-#     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-# else
-#     cd zsh-syntax-highlighting || exit
-#     git pull
-#     cd ..
-# fi
-# if ! [ -d zsh-autosuggestions ]; then
-#     git clone https://github.com/zsh-users/zsh-autosuggestions.git
-# else
-#     cd zsh-autosuggestions || exit
-#     git pull
-#     cd ..
-# fi
-# popd || exit
-
-#### --------------- pyenv ---------------- ####
-# print_header "Install pyenv"
-# if ! [[ -d $HOME/.pyenv ]]; then
-#     git clone https://github.com/pyenv/pyenv.git "$HOME/.pyenv"
-#     pushd "$HOME/.pyenv" || exit
-#     src/configure && make -C src
-#     popd || exit
-# fi
-
-#### --------------- conda ---------------- ####
-# print_header "Install Miniconda"
-# if ! command -v conda &> /dev/null; then
-#     cd "$HOME/Downloads" || exit
-#     MINICONDA_FILE="Miniconda3-latest-Linux-x86_64.sh"
-#     wget "https://repo.anaconda.com/miniconda/$MINICONDA_FILE" -O "$MINICONDA_FILE"
-#     sh "$MINICONDA_FILE" -b -p "$HOME/miniconda3"
-#     "$HOME"/miniconda3/condabin/conda init
-#     popd || exit
-# fi
-
-#### ------------- doom emacs ------------- ####
-# if [ ! -f "$HOME/.emacs.d/bin/doom" ]; then
-#     if [ -d "$HOME/.emacs.d" ]; then
-#         mv "$HOME/.emacs.d" "$HOME/.emacs.d/bkp"
-#     fi
-#     git clone --depth 1 https://github.com/doomemacs/doomemacs "$HOME/.emacs.d"
-#     "$HOME"/.emacs.d/bin/doom install
-# else
-#     "$HOME"/.emacs.d/bin/doom sync
-# fi
